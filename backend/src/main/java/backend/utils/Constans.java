e0b799f9a7eca05edcbce6576a0a76c88aa979a8
Maciek Włodarczyk <195739@edu.p.lodz.pl>
2019-05-10 21:58:42 +0200

    change user roles name to capital letters

--- backend/src/main/java/backend/utils/Constans.java
+++ backend/src/main/java/backend/utils/Constans.java
@@ -2,9 +2,9 @@ package backend.utils;
 
 public abstract class Constans {
     public abstract class USER_ROLES {
-        public static final String USER = "user";
-        public static final String REDACTOR = "redactor";
-        public static final String MODERATOR = "moderator";
-        public static final String ADMIN = "admin";
+        public static final String USER = "USER";
+        public static final String REDACTOR = "REDACTOR";
+        public static final String MODERATOR = "MODERATOR";
+        public static final String ADMIN = "ADMIN";
     }
 }
