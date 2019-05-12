e30384142ce60e01f573ef32328ac9a7bfd1ba9b
Maciek Włodarczyk <195739@edu.p.lodz.pl>
2019-05-12 13:57:39 +0200

    getting example data from Wikipedia

---
+++ backend/src/main/java/backend/Wikipedia.java
@@ -0,0 +1,50 @@
+package backend;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Wikipedia {
+
+    // https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=rainbow
+    public static void getWikipediaApi() {
+
+        try {
+
+            URL url = new URL("https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=rainbow&limit=1 ");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+
+            if (conn.getResponseCode() != 200) {
+                throw new RuntimeException("Failed : HTTP error code : "
+                        + conn.getResponseCode());
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    (conn.getInputStream())));
+
+            String output;
+            System.out.println("Output from Server .... \n");
+            while ((output = br.readLine()) != null) {
+                System.out.println(output);
+            }
+
+            conn.disconnect();
+
+        } catch (MalformedURLException e) {
+
+            e.printStackTrace();
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+
+        }
+
+    }
+
+}
