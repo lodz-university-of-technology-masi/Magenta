f06eebb33a38e1fab7a0f3a66633f79070ebf8a8
Marcin Jaworski <m.a.jaworski96@gmail.com>
2019-05-20 23:14:59 +0200

    test

--- web/src/app/moderator/add-redactor/registration-confirm-password.ts
+++
@@ -1,9 +0,0 @@
-import {SharedConfirmPassword} from '../../shared/utils/shared-confirm-password';
-import {AbstractControl} from '@angular/forms';
-
-export class RegistrationConfirmPassword extends SharedConfirmPassword {
-  matchPassword(abstractControl: AbstractControl): {matchPassword: boolean} {
-    return super.matchPasswordHelper(abstractControl, 'password',
-      'confirmPassword');
-  }
-}
