529e92347ebae2735614ac37b02358c2b914ef11
Marcin Jaworski <m.a.jaworski96@gmail.com>
2019-05-20 23:00:49 +0200

    Refactoring #2

--- web/src/app/moderator/services/redactor-resolve.service.ts
+++ web/src/app/moderator/services/redactor-resolve.service.ts
@@ -5,6 +5,7 @@ import {Observable} from 'rxjs';
 import {User} from '../../models/user/user';
 import {NOT_FOUND_PAGE_URL} from '../../shared/utils/frontend-urls';
 import {RedactorService} from './redactor.service';
+import {UserUpdateData} from '../../models/user/user-update-data';
 
 @Injectable({
   providedIn: 'root'
@@ -15,7 +16,7 @@ export class RedactorResolveService {
               private router: Router) { }
 
   resolve(route: ActivatedRouteSnapshot):
-    Observable<User> | Promise<User> | User {
+    Observable<UserUpdateData> | Promise<UserUpdateData> | UserUpdateData {
     const username = route.params['username'];
     if (username === undefined || username === null) {
       this.router.navigate([NOT_FOUND_PAGE_URL]);
